68a231a04b82ef8b011f4ee61a31b612723ba420
CleuJunior <cleu.junior@gmail.com>
2020-12-12 16:09:26 -0300

    Enserindo seletores

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('input[name=tarefa]')
+const btn = document.querySelector('#botao')
+const lista = document.querySelector('#lista')
+
+let tarefas = [
+    'Estudar PHP',
+    'Estudar CSS',
+    'JavaScript(DOM)',
+    'Estudar Estrutura de Dados',
+    'Se divertir'
+]
+
+console.log(btn)
+console.log(lista)
